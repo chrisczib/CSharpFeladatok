46462d9c7cf3e6a62cf597a7283f71877f5ba3a4
chrisczib <czibere.christopher@gmail.com>
2021-02-13 15:58:44 +0100

    hero game progress

--- ConsoleApp2/HeroGameDir/heroGame.cs
+++ ConsoleApp2/HeroGameDir/heroGame.cs
@@ -26,8 +26,10 @@ namespace ConsoleApp2.HeroGameDir
 			int gameRound = 1;
 			string whoHits = "";
 			bool critical = false;
+			int criticalHit = 0;
 			bool miss = false;
-
+			int missHit = 0;
+			string firstHit = "";
 
 
 
@@ -44,23 +46,63 @@ namespace ConsoleApp2.HeroGameDir
 					{
 						whoHits = heroName;
 						villainHP -= heroHit;
-						//Console.WriteLine("hero hit number {0}", heroHit);
+						firstHit = heroName;
+						Console.WriteLine("hero hit number {0}", heroHit);
 					} else if (villainNum > heroNum)
 					{
 						whoHits = villainName;
 						heroHP -= villainHit;
-						//Console.WriteLine("villain hit number {0}", villainHit);
+						firstHit = villainName;
+						Console.WriteLine("villain hit number {0}", villainHit);
 					}
-					//Console.WriteLine(whoHits);
+					Console.WriteLine(whoHits);
 					
 
 				} else if (gameRound > 1)
 				{
-					/*Console.WriteLine("hero hp {0}", heroHP);
+					if (firstHit == heroName)
+					{
+						heroHit = rnd.Next(6, 9);
+						criticalHit = rnd.Next(1, 101);
+						missHit = rnd.Next(1, 101);
+						
+						//Critical chance 
+						if (criticalHit <= 99)
+						{
+							heroHit *= heroHit;
+							critical = true;
+							Console.WriteLine("critical hit {0}", criticalHit);
+							Console.WriteLine("hero critical {0}", critical);
+							Console.WriteLine("hero critical", critical);
+						}
+						else
+						{
+							heroHit = heroHit;
+							critical = false;
+							Console.WriteLine("critical hit {0}",criticalHit);
+							Console.WriteLine("hero critical {0}", critical);
+						}
+
+						//miss chance
+						if (missHit <= 20)
+						{
+							heroHit = 0;
+							miss = true;
+							Console.WriteLine("miss hit {0}", missHit);
+							Console.WriteLine("hero miss {0}", miss);
+						}
+						else
+						{
+							heroHit = heroHit;
+							miss = false;
+							Console.WriteLine("missHit {0}",missHit);
+							Console.WriteLine("hero miss {0}", miss);
+						}
+
+					}
+					Console.WriteLine("hero hp {0}", heroHP);
 					Console.WriteLine("villain hp {0}", villainHP);
-				
-					 
-					 */
+					
 				}
 				
 
